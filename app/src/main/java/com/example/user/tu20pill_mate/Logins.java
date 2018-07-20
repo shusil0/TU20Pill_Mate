97ca2c639953678a9e14561ea101d0d811677d46
Shusil <shusilshapkota@gmail.com>
2018-07-20 15:28:43 -0400

    Just the homepage layout

---
+++ app/src/main/java/com/example/user/tu20pill_mate/Logins.java
@@ -0,0 +1,13 @@
+package com.example.user.tu20pill_mate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Logins extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logins);
+    }
+}
