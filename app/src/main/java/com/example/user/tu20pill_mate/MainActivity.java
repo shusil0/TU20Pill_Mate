f10e2e18729af239ab3ad9d110a6e26c4c5e9380
Shusil <shusilshapkota@gmail.com>
2018-07-21 19:28:21 -0400

    Connected to Firebase

--- app/src/main/java/com/example/user/tu20pill_mate/MainActivity.java
+++ app/src/main/java/com/example/user/tu20pill_mate/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.user.tu20pill_mate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout l1, l2;
+    Button btnpat;
+    Animation uptodown, downtoup;
+    Button btndoct;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnpat = (Button) findViewById(R.id.buttonpat);
+        btndoct = (Button) findViewById(R.id.buttondoct);
+        l1 = (LinearLayout) findViewById(R.id.l1);
+        l2 = (LinearLayout) findViewById(R.id.l2);
+        uptodown = AnimationUtils.loadAnimation(this, R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this, R.anim.downtoup);
+        l1.setAnimation(uptodown);
+        l2.setAnimation(downtoup);
+
+        //If patient
+        btnpat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent k = new Intent(MainActivity.this, Logins.class);
+                startActivity(k); // runs
+            }
+        });
+
+        // If doctor
+        btndoct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Logins.class);
+                startActivity(i); // runs
+            }
+        });
+
+
     }
 }
